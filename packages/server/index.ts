144e1775659e270844e5863886dd658fdfc74605
Saif Sheikh <ssheikh@shory.com>
2025-12-13 22:52:10 +0400

    chore: refactor

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,19 +1,14 @@
 import express from 'express';
-import type { Request, Response } from 'express';
 import dotenv from 'dotenv';
+import router from './routes';
 
 dotenv.config();
 
 const app = express();
+app.use(express.json());
+app.use(router);
+
 const port = process.env.PORT || 3000;
-
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello WOrld');
-});
-
-app.get('/api/hello', (req: Request, res: Response) => {
-  res.send({ message: 'Hello World' });
-});
 
 app.listen(port, () => {
   console.log(`Server is running on ${port}`);
