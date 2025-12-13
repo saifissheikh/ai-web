52d8eadff756bbf378b8d277b3fbc4c40f7b36b0
Saif Sheikh <ssheikh@shory.com>
2025-12-13 18:31:23 +0400

    feat: setup prettier

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,18 +1,18 @@
-import express from "express";
-import type { Request, Response } from "express";
-import dotenv from "dotenv";
+import express from 'express';
+import type { Request, Response } from 'express';
+import dotenv from 'dotenv';
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello WOrld");
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello WOrld');
 });
 
-app.get("/api/hello", (req: Request, res: Response) => {
-  res.send({ message: "Hello World" });
+app.get('/api/hello', (req: Request, res: Response) => {
+  res.send({ message: 'Hello World' });
 });
 
 app.listen(port, () => {
