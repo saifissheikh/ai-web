9d834020de913666cddf4a96b1200fce98ff5bc7
Saif Sheikh <ssheikh@shory.com>
2025-12-14 17:03:46 +0400

    feat: chatbot

---
+++ packages/client/src/components/chat/ChatBot.tsx
@@ -0,0 +1,48 @@
+import { useRef, useState } from 'react';
+import axios from 'axios';
+import TypingIndicator from './TypingIndicator';
+import type { Message } from './ChatMessages';
+import ChatMessages from './ChatMessages';
+import ChatInput, { type ChatFormData } from './ChatInput';
+
+type ChatResponse = {
+  message: string;
+};
+
+const ChatBot = () => {
+  const [messages, setMessages] = useState<Message[]>([]);
+  const [isBotTyping, setIsBotTyping] = useState(false);
+  const [error, setError] = useState('');
+  const conversationId = useRef(crypto.randomUUID());
+
+  const onSubmit = async ({ prompt }: ChatFormData) => {
+    try {
+      setIsBotTyping(true);
+      setError('');
+      setMessages((prev) => [...prev, { content: prompt, role: 'user' }]);
+      const { data } = await axios.post<ChatResponse>('/api/chat', {
+        prompt,
+        conversationId: conversationId.current,
+      });
+      setMessages((prev) => [...prev, { content: data.message, role: 'bot' }]);
+    } catch (error) {
+      console.error(error);
+      setError('Something went wrong, try again!');
+    } finally {
+      setIsBotTyping(false);
+    }
+  };
+
+  return (
+    <div className="flex flex-col h-full">
+      <div className="flex flex-col flex-1 gap-3 mb-10 overflow-y-auto">
+        <ChatMessages messages={messages} />
+        {isBotTyping && <TypingIndicator />}
+        {error && <p className="text-red-500">{error}</p>}
+      </div>
+      <ChatInput onSubmit={onSubmit} />
+    </div>
+  );
+};
+
+export default ChatBot;
