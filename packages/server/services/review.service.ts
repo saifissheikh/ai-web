445631f89274d462e490380bf4c795d0884e609a
Saif Sheikh <ssheikh@shory.com>
2026-01-07 22:13:58 +0400

    feat: add review summerizer api

---
+++ packages/server/services/review.service.ts
@@ -0,0 +1,33 @@
+import type { Review } from '../generated/prisma/client';
+import { reviewRepository } from '../repositories/review.repository';
+import { llmClient } from '../llm/client';
+import template from '../prompts/summarize-reviews.txt';
+
+export const reviewService = {
+  async getReviews(productId: number): Promise<Review[]> {
+    return reviewRepository.getReviews(productId);
+  },
+
+  async summarizeReviews(productId: number): Promise<string> {
+    const existingSummary = await reviewRepository.getReviewSummary(productId);
+
+    if(existingSummary && existingSummary.expiresAt > new Date()) {
+        return existingSummary.content;
+    }
+    const reviews = await reviewRepository.getReviews(productId, 10);
+    const joinedReviews = reviews.map((r) => r.content).join('\n\n');
+
+    const prompt = template.replace('{{reviews}}', joinedReviews);
+
+    const { text: summary } = await llmClient.generateText({
+      model: 'gpt-4.1',
+      prompt,
+      temperature: 0.2,
+      maxTokens: 500,
+    });
+
+    await reviewRepository.storeReviewSummary(productId, summary);
+
+    return summary;
+  },
+};
